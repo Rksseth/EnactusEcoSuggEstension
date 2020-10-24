6234bc1d62d6c5d889a2dd7e5a53d4998a8203c8
rksseth <rksseth@edu.uwaterloo.ca>
2020-10-24 19:53:59 -0400

    added dictionary to recommend items based off amazon product page, still need to create recommendation code

--- popup.js
+++ popup.js
@@ -1,9 +1,112 @@
+let itemsDict = {
+  'straw': [
+    {
+      'name': '200 Paper Straws',
+      'image': 'https://images-na.ssl-images-amazon.com/images/I/81buBf59KyL._AC_SL1500_.jpg',
+      'cost': '$13.99',
+      'company': 'Alink',
+      'url': 'https://www.amazon.ca/Biodegradable-Assorted-Drinking-Smoothies-Suppliers/dp/B07G97ZLQ3/ref=sr_1_5?dchild=1&keywords=paper+straws&qid=1603574439&sr=8-5' 
+    },
+    {
+      'name': '300 Paper Straws',
+      'image': 'https://images-na.ssl-images-amazon.com/images/I/81zqvuQ1klL._AC_SL1500_.jpg',
+      'cost': '$16.91',
+      'company': 'Zunii',
+      'url': 'https://www.amazon.ca/Zunii-300-Pack-Multi-Color-Biodegradable-Straws/dp/B07V4GWP5L/ref=sr_1_6?dchild=1&gclid=CjwKCAjwoc_8BRAcEiwAzJevtSubDcf9p5d-uohIHRXNrdJbquslx0mzGZD2tzTrQqyLLd5vn3rGixoCjbAQAvD_BwE&hvadid=267117921060&hvdev=c&hvlocphy=9001014&hvnetw=g&hvqmt=e&hvrand=11691619131916741765&hvtargid=kwd-313947535365&hydadcr=21238_9424203&keywords=eco-friendly+straws&qid=1603575433&sr=8-6&tag=googcana-20'
+    },
+    {
+      'name': '100 Bamboo Straws',
+      'image': 'https://images-na.ssl-images-amazon.com/images/I/41IyeOMvvUL._AC_.jpg',
+      'cost': '$14.00',
+      'company': 'TUGS',
+      'url': 'https://www.amazon.ca/TUGS-100-Wheat-Grass-Straws/dp/B084L8RFVG/ref=sr_1_1_sspa?dchild=1&gclid=CjwKCAjwoc_8BRAcEiwAzJevtSubDcf9p5d-uohIHRXNrdJbquslx0mzGZD2tzTrQqyLLd5vn3rGixoCjbAQAvD_BwE&hvadid=267117921060&hvdev=c&hvlocphy=9001014&hvnetw=g&hvqmt=e&hvrand=11691619131916741765&hvtargid=kwd-313947535365&hydadcr=21238_9424203&keywords=eco-friendly+straws&qid=1603575433&sr=8-1-spons&tag=googcana-20&psc=1&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUFTTEU2MkFMN1ExTUgmZW5jcnlwdGVkSWQ9QTA3OTYyNDMxSFlDRUFKWEpEWUhGJmVuY3J5cHRlZEFkSWQ9QTA4NjM0MDMyVFdCRTRMSTk1WkE3JndpZGdldE5hbWU9c3BfYXRmJmFjdGlvbj1jbGlja1JlZGlyZWN0JmRvTm90TG9nQ2xpY2s9dHJ1ZQ=='
+    }
+  ],
+  'soap': [
+    {
+      'name': 'Starter Pack',
+      'image': 'https://cdn.shopify.com/s/files/1/0082/5701/1759/products/earth-suds-travel-pack-top-beside_final_1800x1800.png?v=1592338160',
+      'cost': '$13.99',
+      'company': 'Earth Suds',
+      'url': 'https://earthsuds.co/collections/all/products/starter-pack' 
+    },
+    {
+      'name': 'Eco-friendly Body Wash',
+      'image': 'https://images-na.ssl-images-amazon.com/images/I/61St-u3WfyL._AC_SL1500_.jpg',
+      'cost': '$20.37',
+      'company': 'Ethique',
+      'url': 'https://www.amazon.ca/Ethique-Eco-Friendly-Bodywash-Orange-Vanilla/dp/B07J2S2GVM/ref=sr_1_12?dchild=1&keywords=eco+friendly+body+wash&qid=1603577621&sr=8-12' 
+    },
+    {
+      'name': 'Hypoallergenic & Eco-friendly Body Wash',
+      'image': 'https://images-na.ssl-images-amazon.com/images/I/61VOA-pTD6L._AC_SL1080_.jpg',
+      'cost': '$8.99',
+      'company': 'ATTITUDE',
+      'url': 'https://www.amazon.ca/VERIFIED-Hypoallergenic-Dermatologist-Tear-Free-Collection/dp/B07DQ76VTM/ref=sr_1_8?dchild=1&keywords=eco+friendly+body+wash&qid=1603577695&sr=8-8' 
+    }
+  ],
+  'cutlery': [
+    {
+      'name': 'Disposable Wooden Cutlery',
+      'image': 'https://images-na.ssl-images-amazon.com/images/I/61Q4XuLYN8L._AC_SL1169_.jpg',
+      'cost': '$24.99',
+      'company': 'DYA',
+      'url': 'https://www.amazon.ca/DYA-Disposable-Wooden-Cutlery-Biodegradable/dp/B07KWN1ZFV/ref=sr_1_1_sspa?dchild=1&keywords=eco+friendly+cutlery&qid=1603577968&sr=8-1-spons&psc=1&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUFUQTI0OFZWWVgyVjcmZW5jcnlwdGVkSWQ9QTA1ODg5ODAxWTJXSDMwTjhIWFJPJmVuY3J5cHRlZEFkSWQ9QTA4NzkxNjhHUTBaUUgzR0EwVFomd2lkZ2V0TmFtZT1zcF9hdGYmYWN0aW9uPWNsaWNrUmVkaXJlY3QmZG9Ob3RMb2dDbGljaz10cnVl' 
+    },
+    {
+      'name': 'Plant-based Cutlery',
+      'image': 'https://images-na.ssl-images-amazon.com/images/I/81mg6tx0heL._AC_SL1500_.jpg',
+      'cost': '$19.44',
+      'company': 'Mozaik',
+      'url': 'https://www.amazon.ca/Mozaik-PM80CAM-CPLA-Cutlery-Natural/dp/B07R18CLNR/ref=sr_1_6?dchild=1&keywords=eco+friendly+cutlery&qid=1603578095&sr=8-6' 
+    },
+    {
+      'name': 'Biodegradable Cutlery',
+      'image': 'https://images-na.ssl-images-amazon.com/images/I/61AHQh7XLgL._AC_SL1500_.jpg',
+      'cost': '$17.99',
+      'company': 'Culinware',
+      'url': 'https://www.amazon.ca/100-Biodegradable-Cutlery-Compostable-Tableware/dp/B07XLF3TKB/ref=sr_1_2_sspa?dchild=1&keywords=eco+friendly+cutlery&qid=1603577968&sr=8-2-spons&psc=1&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUFUQTI0OFZWWVgyVjcmZW5jcnlwdGVkSWQ9QTA1ODg5ODAxWTJXSDMwTjhIWFJPJmVuY3J5cHRlZEFkSWQ9QTAyMzg5NzgzQk8xNExNVVJCODEmd2lkZ2V0TmFtZT1zcF9hdGYmYWN0aW9uPWNsaWNrUmVkaXJlY3QmZG9Ob3RMb2dDbGljaz10cnVl' 
+    }
+  ]
+}
+let keysDict = {
+  'straw': 'straw',
+  'soap': 'soap',
+  'clean': 'soap',
+  'bodywash': 'soap',
+  'cutlery': 'cutlery',
+  'spoon': 'cutlery',
+  'fork': 'cutlery',
+  "utensil": 'cutlery'
+}
+
 let element = document.getElementById("product-name");
+let price = document.getElementById('price1');
 
 function updatePopup(){
   chrome.storage.sync.get(['product'], function(data) {
-    element.innerText = data.product;
+    let productTitle = data.product; // from amazon product title
 
+    // choose the key based on amazon page
+    key = "soap";
+    let items = itemsDict[key];
+
+    // Loop over 3 items to change 
+    for (let i = 1; i < 4; i++) {
+      let item = items[i-1];
+
+      let img = document.getElementById('img' + i);
+      img.src = item.image;
+
+      let product = document.getElementById('product' + i);
+      product.innerText = item.name;
+
+      let company = document.getElementById('company' + i);
+      company.innerText = item.company;
+
+      let price = document.getElementById('price' + i);
+      price.innerText = item.cost;
+    }
   });
 }
 
